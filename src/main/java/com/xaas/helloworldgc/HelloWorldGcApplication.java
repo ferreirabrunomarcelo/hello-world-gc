3b9d94f1b0f5ceb33ed9be587b48173344d0da88
Bruno Marcelo Ferreira <ferreira.brunomarcelo@gmail.com>
2022-11-09 15:03:16 -0300

    Initial commit

---
+++ src/main/java/com/xaas/helloworldgc/HelloWorldGcApplication.java
@@ -0,0 +1,13 @@
+package com.xaas.helloworldgc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldGcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldGcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xaas/helloworldgc/HelloWorldGcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xaas.helloworldgc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldGcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
